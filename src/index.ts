f1fa49a5e5a0b00ca7a07782d87c7666f6c32527
Vivek Tiwari <vivek.t74@gmail.com>
2025-12-23 10:53:19 +0530

    Final v1.4

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,16 @@ import compression from 'compression';
 import rateLimit from 'express-rate-limit';
 import pool from './db.js';
 import dotenv from 'dotenv';
-import { errorHandler } from '../src/middleware/errorHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
 import { logger } from '../src/utils/logger.js';
 
 // Routes
-import stateRoutes from '../src/routes/states.routes.js';
-import constituencyRoutes from '../src/routes/constituencies.routes.js';
-import electionRoutes from '../src/routes/elections.routes.js';
-import boothRoutes from '../src/routes/booths.routes.js';
-import candidateRoutes from '../src/routes/candidates.routes.js';
-import boothAnalysisRoutes from '../src/routes/booth-analysis.routes.js'
+import stateRoutes from './routes/states.routes.js';
+import constituencyRoutes from './src/routes/constituencies.routes.js';
+import electionRoutes from './routes/elections.routes.js';
+import boothRoutes from './routes/booths.routes.js';
+import candidateRoutes from './routes/candidates.routes.js';
+import boothAnalysisRoutes from './routes/booth-analysis.routes.js'
 
 // Load environment variables FIRST
 dotenv.config();
